6c3fb7df87455a14993828e00eaca042acbfad1f
Massimiliano Mirra <bard@hyperstruct.net>
2022-12-18 17:06:50 +0000

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use decentralized_bookmarks::adapters::{http_api, memory_event_store::MemoryEventStore};
+use dumb_channel_decentralized_sync::adapters::{http_api, memory_event_store::MemoryEventStore};
 
 fn main() {
     http_api::run("localhost:9111", Arc::new(MemoryEventStore::new()));
