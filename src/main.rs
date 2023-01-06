f6e6b81b691d33f47c24d8485f456aa254759bd3
Massimiliano Mirra <bard@hyperstruct.net>
2023-01-06 13:42:27 +0000

    make port configurable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use std::sync::Arc;
 
+use clap::Parser;
 use decentrasync::adapters::{http_api, memory_event_store::MemoryEventStore};
 
+#[derive(Parser, Debug)]
+#[command(about)]
+struct Args {
+    #[arg(short, long, default_value_t = 9111)]
+    port: u16,
+}
+
 fn main() {
-    http_api::run("localhost:9111", Arc::new(MemoryEventStore::new()));
+    let args = Args::parse();
+    http_api::run(
+        format!("localhost:{}", args.port).as_str(),
+        Arc::new(MemoryEventStore::new()),
+    );
 }
