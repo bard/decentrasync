91457e7c8a5d43b3a7d9752acf61ef37d2166b68
Massimiliano Mirra <bard@hyperstruct.net>
2023-02-01 22:41:23 +0000

    handle errors in axum adapter

--- src/adapters/http_api_axum.rs
+++ src/adapters/http_api_axum.rs
@@ -46,13 +46,15 @@ pub fn create_router(
 }
 
 async fn root(State(_state): State<Arc<ServiceDependencies>>) -> impl IntoResponse {
-    let asset = Asset::get("index.html").unwrap();
-
-    Response::builder()
-        .status(StatusCode::OK)
-        .header(header::CONTENT_TYPE, "text/html")
-        .body(body::boxed(Full::from(asset.data)))
-        .unwrap()
+    if let Some(asset) = Asset::get("index.html") {
+        Response::builder()
+            .status(StatusCode::OK)
+            .header(header::CONTENT_TYPE, "text/html")
+            .body(body::boxed(Full::from(asset.data)))
+            .unwrap()
+    } else {
+        (StatusCode::NOT_FOUND).into_response()
+    }
 }
 
 #[derive(Serialize)]
@@ -67,20 +69,23 @@ struct ReadBookmarksResponseBookmarkEntry {
 }
 
 async fn read_bookmarks(State(state): State<Arc<ServiceDependencies>>) -> impl IntoResponse {
-    let bookmarks = app::query::read_bookmarks(state.read_model.clone()).unwrap();
-    (
-        StatusCode::OK,
-        Json(ReadBookmarksResponse {
-            bookmarks: bookmarks
-                .iter()
-                .map(|b| ReadBookmarksResponseBookmarkEntry {
-                    id: b.id.clone(),
-                    url: b.url.clone(),
-                    title: b.title.clone(),
-                })
-                .collect(),
-        }),
-    )
+    match app::query::read_bookmarks(state.read_model.clone()) {
+        Some(bookmarks) => (
+            StatusCode::OK,
+            Json(ReadBookmarksResponse {
+                bookmarks: bookmarks
+                    .iter()
+                    .map(|b| ReadBookmarksResponseBookmarkEntry {
+                        id: b.id.clone(),
+                        url: b.url.clone(),
+                        title: b.title.clone(),
+                    })
+                    .collect(),
+            }),
+        )
+            .into_response(),
+        _ => (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+    }
 }
 
 #[derive(Deserialize)]
@@ -100,7 +105,7 @@ async fn create_bookmark(
 ) -> impl IntoResponse {
     let id = Uuid::new_v4().to_string();
 
-    app::command::create_bookmark(
+    match app::command::create_bookmark(
         app::Bookmark {
             id: id.clone(),
             url: payload.url,
@@ -109,13 +114,14 @@ async fn create_bookmark(
         state.event_store.clone(),
         state.read_model.clone(),
         state.clock.clone(),
-    )
-    .unwrap();
-
-    (
-        StatusCode::CREATED,
-        Json(CreateBoomarkResponsePayload { id }),
-    )
+    ) {
+        Ok(()) => (
+            StatusCode::CREATED,
+            Json(CreateBoomarkResponsePayload { id }),
+        )
+            .into_response(),
+        _ => (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+    }
 }
 
 #[derive(Deserialize)]
@@ -128,31 +134,31 @@ async fn update_bookmark_title(
     Path(id): Path<String>,
     Json(payload): Json<UpdateBookmarkTitleRequestPayload>,
 ) -> impl IntoResponse {
-    app::command::update_bookmark_title(
+    match app::command::update_bookmark_title(
         id,
         payload.title,
         state.event_store.clone(),
         state.read_model.clone(),
         state.clock.clone(),
-    )
-    .unwrap();
-
-    (StatusCode::NO_CONTENT, ())
+    ) {
+        Ok(()) => (StatusCode::NO_CONTENT, ()).into_response(),
+        _ => (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+    }
 }
 
 async fn delete_bookmark(
     State(state): State<Arc<ServiceDependencies>>,
     Path(id): Path<String>,
 ) -> impl IntoResponse {
-    app::command::delete_bookmark(
+    match app::command::delete_bookmark(
         id,
         state.event_store.clone(),
         state.read_model.clone(),
         state.clock.clone(),
-    )
-    .unwrap();
-
-    (StatusCode::OK, ())
+    ) {
+        Ok(()) => (StatusCode::OK, ()).into_response(),
+        _ => (StatusCode::INTERNAL_SERVER_ERROR).into_response(),
+    }
 }
 
 #[derive(Serialize)]
